e1d212f6d0aeecd134b961467b34eb504b0ace76
sidd <supersid2001@gmail.com>
2023-07-12 20:49:59 -0400

    Made list stacked

--- Assets/MultipleListsContainer.cs
+++ Assets/MultipleListsContainer.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class MultipleListsContainer : MonoBehaviour
 {
     public List<GameObject> itemsMenus;
-    // Start is called before the first frame update
+
+    int currIndex = 0;
+
+    [SerializeField]
+    bool isMenu = false;
+
+    void Update()
+    {
+        if (isMenu)
+        {
+            //if eye gaze not on task objects then do fade out currentindex
+            //else fade back in currentindex 
+        }
+    }
     public void SetMenuActive(int index)
     {
+        currIndex = index;
         for(int i = 0; i < itemsMenus.Count; i++)
         {
             if(i == index)
