88159092a1d36f6957dd3c465d588294e576fcdb
sidd <supersid2001@gmail.com>
2023-08-01 22:49:30 -0400

    Modified line opacity

--- Assets/Plugins/ARUI/Scripts/Center_of_Objs.cs
+++ Assets/Plugins/ARUI/Scripts/Center_of_Objs.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Shapes;
 
 public class Center_of_Objs : MonoBehaviour
 {
     // Debug only
     public List<GameObject> objs;
 
+    public GameObject LinePrefab;
+
     public float movementSpeed = 1.0f;
     // Debug end
 
     Dictionary<string, GameObject> objsDict = new Dictionary<string, GameObject>();
+    Dictionary<string, GameObject> linesDict = new Dictionary<string, GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -38,16 +42,28 @@ public class Center_of_Objs : MonoBehaviour
 
     public void RemoveObj(string key){
         objsDict.Remove(key);
+        Destroy(linesDict[key]);
+        linesDict.Remove(key);
         //Delete corresponding lines
     }
 
     public void ClearObjs(){
         objsDict.Clear();
+        foreach(KeyValuePair<string, GameObject> line in linesDict)
+        {
+            Destroy(line.Value);
+        }
+        linesDict.Clear();
         //Delete all lines here
     }
 
     public void AddObj(string key, GameObject obj){
         objsDict.Add(key, obj);
+        GameObject pointerObj = Instantiate(LinePrefab, Vector3.zero, this.transform.rotation);
+        linesDict.Add(key, pointerObj);
+        Line pointer = pointerObj.GetComponent<Line>();
+        pointer.Start = transform.position;
+        pointer.End = obj.transform.position;
         //Add new line here
     }
 
